7e6f4971ed1036f69ddb80f3e63c68d36158c880
silje <angelviksilje@gmail.com>
2022-11-03 19:16:27 +0100

    Moodle: Tailoring Your Environment made improvements to src/joker.js

--- src/js/joker.js
+++ src/js/joker.js
@@ -1,3 +1,7 @@
-import joker from "one-liner-joke";
+import joker from "give-me-a-joke";
 
-console.log(joker.getRandomJoke().body);
+export function dadJoke() {
+    return new Promise(function(resolve, reject) {
+        joker.getRandomDadJoke(resolve);
+    })
+}
